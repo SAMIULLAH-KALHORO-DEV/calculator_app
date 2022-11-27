1562334bf1b2692fd4d41a4088a3249d9d077e91
SAMIULLAH-KALHOR0 <91752628+SAMIULLAH-KALHOR0@users.noreply.github.com>
2022-11-27 21:31:53 +0500

    Calculator App using Dart and Flutter Frame work

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'calculator.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Calculator());
+  }
+}
